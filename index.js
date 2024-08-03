2617dde8d8b67383a275867dedd232b8c4da4e4d
gaurav <gaurav.gangde21@vit.edu>
2024-08-03 13:38:37 +0530

    Changes and fixes required for set up

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const dotenv = require('dotenv');
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+
+
+// const dotenv = require("dotenv").config();
+// FILE IS NOT MENTIONED FOR .ENV
+// require("dotenv").config();
+
 
 const app = express()
 const Routes = require("./routes/route.js")
 
-const PORT = process.env.PORT || 5000
+
 
 dotenv.config();
+// the position of the below is changed so that it can read env variables
+const PORT = process.env.PORT || 5000
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
--- models/orderSchema.js
+++ models/orderSchema.js
@@ -1,120 +1,121 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema(
+const orderSchema = new mongoose.Schema({
+  buyer: {
+    type: mongoose.Schema.ObjectId,
+    ref: "customer",
+    required: true,
+  },
+  shippingData: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    pinCode: {
+      type: Number,
+    },
+    phoneNo: {
+      type: Number,
+      required: true,
+    },
+  },
+  orderedProducts: [
     {
-        buyer: {
-            type: mongoose.Schema.ObjectId,
-            ref: "customer",
-            required: true,
+      productName: {
+        type: String,
+      },
+      price: {
+        mrp: {
+          type: Number,
         },
-        shippingData: {
-            address: {
-                type: String,
-                required: true,
-            },
-            city: {
-                type: String,
-                required: true,
-            },
-            state: {
-                type: String,
-                required: true,
-            },
-            country: {
-                type: String,
-                required: true,
-            },
-            pinCode: {
-                type: Number,
-                
-            },
-            phoneNo: {
-                type: Number,
-                required: true,
-            },
+        cost: {
+          type: Number,
         },
-        orderedProducts: [{
-            productName: {
-                type: String
-            },
-            price: {
-                mrp: {
-                    type: Number
-                },
-                cost: {
-                    type: Number
-                },
-                discountPercent: {
-                    type: Number
-                }
-            },
-            subcategory: {
-                type: mongoose.Schema.Types.ObjectId,
-            },
-            productImage: {
-                type: String
-            },
-            category: {
-                type: String
-            },
-            description: {
-                type: String
-            },
-            tagline: {
-                type: String
-            },
-            quantity: {
-                type: Number
-            },
-            seller: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'seller'
-            },
-        }],
-        paymentInfo: {
-            id: {
-                type: String,
-                required: true,
-            },
-            status: {
-                type: String,
-                required: true,
-            },
+        discountPercent: {
+          type: Number,
         },
-        paidAt: {
-            type: Date,
-            required: true,
-        },
-        productsQuantity: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        taxPrice: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        shippingPrice: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-        totalPrice: {
-            type: Number,
-            required: true,
-            default: 20,
-        },
-        orderStatus: {
-            type: String,
-            required: true,
-            
-        },
-        deliveredAt: Date,
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-    });
+      },
+      subcategory: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+      productImage: {
+        type: String,
+      },
+      category: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+      tagline: {
+        type: String,
+      },
+      quantity: {
+        type: Number,
+      },
+      seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "seller",
+      },
+    },
+  ],
+  paymentInfo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  paidAt: {
+    type: Date,
+    required: true,
+  },
+  productsQuantity: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  taxPrice: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  shippingPrice: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    default: 20,
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+  },
+  deliveredAt: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-module.exports = mongoose.model("customer", orderSchema);
+// module.exports = mongoose.model("customer", orderSchema);
+// here the name of model is is incorrect to customer
+module.exports = mongoose.model("order", orderSchema);
